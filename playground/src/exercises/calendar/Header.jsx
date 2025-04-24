ff875966c0eb320269a4d5766d72c70948dac01c
spider <burkstremane@gmail.com>
2025-04-24 12:06:03 -0600

    padded num bug

--- playground/src/exercises/calendar/Header.jsx
+++ playground/src/exercises/calendar/Header.jsx
@@ -3,7 +3,8 @@ import { useEffect, useState } from "react"
 
 export const Header = () => {
   const [t,setTime] = useState(time());
-  const {dow,month,date,hour,minute,second,context} = t
+  const {dow,month,date,hour,second,context} = t
+  const minute = t.time.minute
   useEffect(() => {
     const nextMinute = (60 - second) * 1000;
     const getTime = () => time();
@@ -16,7 +17,7 @@ export const Header = () => {
     }
     timeoutId = setTimeout(() => alignTime(),nextMinute)
     return () => {
-      console.log('timout cleared')
+      console.log('timout cleared', typeof String(minute),minute)
       clearTimeout(timeoutId)
       clearInterval(intervalId)
     }
@@ -28,7 +29,7 @@ export const Header = () => {
         <div className="dayToday">
           <div className="today">
             <div className="dow dayPart">{dow}</div>
-            <div className="date dayPart">{date}</div>
+            <div className="date dayPart">{String(date)}</div>
             <div className="month dayPart">{month}</div>
             <div className="expand-widget">
               <div className="icon"><svg xmlns="http://www.w3.org/2000/svg" width="16px" height="16px" viewBox="0 0 16 16" pid="m9ui3rf5-01HG6ARZM9Y7"><path fill-rule="evenodd" d="M1.75 2.5a.75.75 0 000 1.5h10.5a.75.75 0 000-1.5H1.75zm4 5a.75.75 0 000 1.5h8.5a.75.75 0 000-1.5h-8.5zm0 5a.75.75 0 000 1.5h8.5a.75.75 0 000-1.5h-8.5zM2.5 7.75a.75.75 0 00-1.5 0v6a.75.75 0 001.5 0v-6z" fill="#000" pid="m9ui3rf5-02DH23I72TEW"></path></svg></div>
@@ -37,9 +38,9 @@ export const Header = () => {
           <div className="time">
             <div className="icon"><svg xmlns="http://www.w3.org/2000/svg" width="16px" height="16px" viewBox="0 0 16 16" pid="m9ui3rf5-00EF9TMYCIJU"><path fill-rule="evenodd" d="M1.5 8a6.5 6.5 0 1113 0 6.5 6.5 0 01-13 0zM8 0a8 8 0 100 16A8 8 0 008 0zm.5 4.75a.75.75 0 00-1.5 0v3.5a.75.75 0 00.471.696l2.5 1a.75.75 0 00.557-1.392L8.5 7.742V4.75z" fill="#000" pid="m9ui3rf5-01NLICRKT6DI"></path></svg></div>
             <div className="curtime">
-              <div className="hour">{hour}</div>
+              <div className="hour">{String(hour)}</div>
                 <div className="divider">:</div>
-                <div className="minute">{minute}</div>
+                <div className="minute">{String(minute)}</div>
                 <div className="context">{context.toLowerCase()}</div>
               </div>
           </div>
