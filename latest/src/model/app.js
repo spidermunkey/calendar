124fc7c1ab294f1806128d3c230750bfd8ba2ca6
spider <burkstremane@gmail.com>
2025-05-11 17:10:50 -0600

    cleanup

--- latest/src/model/app.js
+++ latest/src/model/app.js
@@ -4,18 +4,45 @@ export const createAppModel = () => {
   const date = new Date();
   return {
     name:'My first app',
-    birthdays: birthdayStore,
     currentMonth: date.getMonth(),
     currentDay: date.getDate(),
-
     today: {
       month: date.getMonth(), 
       year: date.getFullYear(),
       day: date.getDate(),
     },
 
-    async dayData(day = this.currentDay , month = this.currentMonth){
-      console.log(day,month)
+    birthdays: {
+      data:birthdayStore,
+      async getData() {
+        return this.data.getData();
+      },
+      async isToday(){
+        const data = await this.getData();
+        const today = new Date();
+        const month = today.getMonth() + 1;
+        const day = today.getDate();
+        const found = data.filter(bday => bday.month === month && bday.day === day);
+        return found;
+      },
+      async isThisMonth() {
+        const date = new Date();
+        const thisMonth = await this.getByMonth(date.getMonth())
+        return thisMonth
+      },
+      async getByDate({ month, day }){
+        const data = await this.getData();
+        const found = data.filter(bday => bday.month == month + 1 && bday.day == day);
+        return found;
+      },
+      async getByMonth(monthIndex){
+        const data = await this.getData();
+        const isThisMonth = data.filter(bday => bday.month == monthIndex + 1);
+        return isThisMonth;
+      },
+    },
+
+    async getDay(day = this.currentDay , month = this.currentMonth){
       const birthdaysToday = await this.birthdays.getByDate({month,day})
       const birthdaysThisMonth = await this.birthdays.getByMonth(month)
       return {
