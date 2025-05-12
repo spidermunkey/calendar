e3da6db4a3f3583107ac9f12c39fb10f474a949a
spider <burkstremane@gmail.com>
2025-05-11 18:36:29 -0600

    update timer styles

--- latest/src/dashboard/timer/Timer.jsx
+++ latest/src/dashboard/timer/Timer.jsx
@@ -19,19 +19,20 @@ export const Timer = ({ name = 'focus', time={hours:0,minutes:25,seconds:0}, typ
   const { hours, minutes, seconds } = currentTime;
   console.log(state)
   return (
-    <div className="timer">
+    <div className="timer timer-element">
+      {state === 'running' ? <div className="activity-sensor"></div>
+      :''}
       <div className="info">
+        <div className="timer-title">{timer.title}</div>
+        
         <div className="current-time">
           {hours > 1 ? <><div className="hours">{hours}</div><div className="divider">:</div></> : ''}
           <div className="minutes">{minutes < 10 ? String(minutes).padStart(2,'0') : minutes}</div>
           <div className="divider">:</div>
           <div className="seconds">{seconds < 10 ? String(seconds).padStart(2,'0') : seconds}</div>
         </div>
-        <div className="timer-title">{timer.title}</div>
 
       </div>
-
-
       <div className="stopwatch-controls" >
       {state === 'stopped' 
         ? <div className="btn-control play" onClick={play}>play</div>
