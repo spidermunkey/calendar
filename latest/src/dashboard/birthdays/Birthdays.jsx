24bfc4e78b62de446238251223d3842174d5f2ed
spider <burkstremane@gmail.com>
2025-05-12 07:28:44 -0600

    align birthday modal styles

--- latest/src/dashboard/birthdays/Birthdays.jsx
+++ latest/src/dashboard/birthdays/Birthdays.jsx
@@ -3,12 +3,20 @@ import { useEffect, useState } from "react";
 import { CreateForm } from "./CreateBirthdayForm";
 import { NamedList} from './NameList';
 import { BtnAdd } from "./AddButton";
+import { CloseIcon } from "icons";
 
-export const Birthdays = ({birthdays,add,update,remove,currentMonth}) => {
+export const Birthdays = ({ birthdays, add , update , remove , currentMonth}) => {
   const [formActive,setFormActive] = useState(false);
   useEffect(update,[birthdays])
   return (
-    <div className='birthdays flex-col p-12'>
+    <div className='interface-modal birthdays'>
+      <div className="interface-header">
+        <div className="interface-title">Birthdays</div>
+        <div className="btn-close">
+          <div className="label">close</div>
+          <div className="icon"><CloseIcon/></div>
+        </div>
+      </div>
       <CreateForm add={add} isActive={formActive} setActive={setFormActive}/>
       <BtnAdd onClick={() => setFormActive(!formActive)}/>
       <NamedList currentMonth={currentMonth} birthdays={birthdays} onDelete={remove}/>
