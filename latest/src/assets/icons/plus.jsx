99aaaa404908d068f5fcd4b3ac723264a7bda16f
spider <burkstremane@gmail.com>
2025-05-27 16:36:16 -0600

    adding event modal

--- latest/src/assets/icons/plus.jsx
+++ latest/src/assets/icons/plus.jsx
@@ -1,4 +1,4 @@
 export const PlusIcon = () => (<svg width="24px" height="24px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" pid="m91x24ts-01LWS811JV39">
-<path d="M12 17V7" stroke="black" stroke-width="1.5" stroke-linecap="round" pid="m91x24ts-00PZJJP4KRQZ"></path>
-<path d="M7 12L17 12" stroke="black" stroke-width="1.5" stroke-linecap="round" pid="m91x24ts-01D8ZJNF6NYJ"></path>
+<path d="M12 17V7" stroke-width="1.5" stroke-linecap="round" pid="m91x24ts-00PZJJP4KRQZ"></path>
+<path d="M7 12L17 12" stroke-width="1.5" stroke-linecap="round" pid="m91x24ts-01D8ZJNF6NYJ"></path>
 </svg>) 
--- latest/src/dashboard/calendar/EventModal.jsx
+++ latest/src/dashboard/calendar/EventModal.jsx
@@ -1,7 +1,9 @@
+import { PlusIcon } from "../../assets/icons/plus"
 export const EventModal = () => {
   return (
   <>
     <div className="interface-modal events">
+
       <div className="interface-header">
         <div className="interface-title">Events</div>
       </div>
@@ -11,17 +13,35 @@ export const EventModal = () => {
         <div className="section-data">
           <div className="bullet">none</div>
         </div>
+        <div className="btn-add-event">
+          <div className="text">Add Daily Event</div>
+          <div className="btn-add-icon" handle="daily">
+            <PlusIcon/>
+          </div>
+        </div>
       </div>
       <div className="section weekly">
         <div className="section-title">This Week</div>
         <div className="section-data">
           <div className="bullet">none</div>
         </div>
+        <div className="btn-add-event">
+          <div className="text">Add Weekly Event</div>
+          <div className="btn-add-icon" handle="daily">
+            <PlusIcon/>
+          </div>
+        </div>
       </div>
       <div className="section monthly">
         <div className="section-title">This Month</div>
         <div className="section-data">
           <div className="bullet">none</div>
+        </div>
+        <div className="btn-add-event">
+          <div className="text">Add Monthly Event</div>
+          <div className="btn-add-icon" handle="daily">
+            <PlusIcon/>
+          </div>
         </div>
       </div>
       
